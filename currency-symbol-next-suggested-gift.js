84a765ce0e0455d35c884760d3daca3020e0de2f
Corin Hartley-Pearce <35596778+corinEN@users.noreply.github.com>
2022-07-06 15:45:13 +0100

    Update currency-symbol-next-suggested-gift.js

--- currency-symbol-next-suggested-gift.js
+++ currency-symbol-next-suggested-gift.js
@@ -1,7 +1,7 @@
 // NOTE: this requires the jQuery library
 // The Next Suggested Gift can change the radio donation amounts on a donation page
 // But it loses the currency symbol. This code adds the currency symbol (set as £ in this example)
-// to the amount label, if the first character is a number (e.g. 10 becomes £10 but Other stays the same)
+// to the amount label, if the first character is a number (e.g. "10" becomes "£10" but "£20" or "Other" stays the same)
 // setTimeout is needed to ensure this code runs after the NSG has done what it needs to do
 <script>
 $(function() {
